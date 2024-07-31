47dcdea6ea0052d033c182651267a63224ddddcf
white0dew <1373685219@qq.com>
2024-07-31 19:56:22 +0800

    bing

--- lib/bing-seo.ts
+++ lib/bing-seo.ts
@@ -6,17 +6,23 @@ export const submitUrlsToIndexNow = async (urls: string[]) => {
     host: "offernow.cn",
     key: "e5d66260a41d4cca9e9b369be3efaa81",
     keyLocation: "https://offernow.cn/e5d66260a41d4cca9e9b369be3efaa81.txt",
-    urlList: url,
+    urlList: urls,
   };
-  console.log("submitUrlsToIndexNow", urls);
+  console.log("submitUrlsToIndexNow", payload);
 
   try {
+    // 设置超时时间
+    // Set up AbortController and timeout
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 50000); // Set timeout to 5000ms (5 seconds)
+
     const response = await fetch(url, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json; charset=utf-8",
+        "Content-Type": "application/json;",
       },
       body: JSON.stringify(payload),
+      signal: controller.signal, // Attach the abort signal
     });
 
     if (!response.ok) {
